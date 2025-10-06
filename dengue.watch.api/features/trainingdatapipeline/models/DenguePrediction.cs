86537538649ca6d73327e3edeb3465f7ae3db22c
SejiMe <atienza.seji@gmail.com>
2025-10-06 22:33:14 +0800

    Remove Dengue alert feature, including models, DTOs, endpoints, service, and SignalR hub. This cleanup eliminates unused code related to dengue alerts from the project.

--- dengue.watch.api/features/trainingdatapipeline/models/DenguePrediction.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.ML.Data;
-namespace dengue.watch.api.features.trainingdatapipeline.models;
-
-public class DenguePrediction
-{
-    [ColumnName("Score")]
-    public float PredictedCaseCount { get; set; }
-}
