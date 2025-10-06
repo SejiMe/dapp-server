370f63bb2c96646b31a09aef876ccfd04f18e6c5
SejiMe <atienza.seji@gmail.com>
2025-10-06 22:34:51 +0800

    Add migration for predicted weekly dengue cases data
    - Created migration to add the `predicted_weekly_dengue_cases` table with necessary columns and constraints.
    - Updated model snapshot to include the new `PredictedWeeklyDengueCase` entity and its relationships.
    - Introduced `LaggedDengueCausalData` model for enhanced data handling in ML predictions.

---
+++ dengue.watch.api/Migrations/20251006140824_AddPredictionDengueCasesData.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dengue.watch.api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPredictionDengueCasesData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "predicted_weekly_dengue_cases",
+                columns: table => new
+                {
+                    prediction_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    iso_week = table.Column<int>(type: "integer", nullable: false),
+                    iso_year = table.Column<int>(type: "integer", nullable: false),
+                    predicted_value = table.Column<int>(type: "integer", nullable: false),
+                    predicted_date = table.Column<DateOnly>(type: "date", nullable: false),
+                    PsgcCode = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_predicted_weekly_dengue_cases", x => x.prediction_id);
+                    table.ForeignKey(
+                        name: "FK_predicted_weekly_dengue_cases_administrative_areas_PsgcCode",
+                        column: x => x.PsgcCode,
+                        principalTable: "administrative_areas",
+                        principalColumn: "psgc_code",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_predicted_weekly_dengue_cases_PsgcCode",
+                table: "predicted_weekly_dengue_cases",
+                column: "PsgcCode");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "predicted_weekly_dengue_cases");
+        }
+    }
+}
