ca3783f31f3656b346790a085e6ee1430d0cca87
SejiMe <atienza.seji@gmail.com>
2025-10-29 05:58:32 +0800

    Add CSV

---
+++ dengue.watch.api/Migrations/20251019085504_RenameToLagged.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dengue.watch.api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameToLagged : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "iso_year",
+                table: "predicted_weekly_dengue_cases",
+                newName: "lagged_iso_year");
+
+            migrationBuilder.RenameColumn(
+                name: "iso_week",
+                table: "predicted_weekly_dengue_cases",
+                newName: "lagged_iso_week");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "lagged_iso_year",
+                table: "predicted_weekly_dengue_cases",
+                newName: "iso_year");
+
+            migrationBuilder.RenameColumn(
+                name: "lagged_iso_week",
+                table: "predicted_weekly_dengue_cases",
+                newName: "iso_week");
+        }
+    }
+}
---
+++ dengue.watch.api/Migrations/20251022205108_AddedStatisticsRelated.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dengue.watch.api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedStatisticsRelated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "confidence_percentage",
+                table: "predicted_weekly_dengue_cases",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<float>(
+                name: "lower_bound",
+                table: "predicted_weekly_dengue_cases",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<double>(
+                name: "probability_of_outbreak",
+                table: "predicted_weekly_dengue_cases",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "risk_level",
+                table: "predicted_weekly_dengue_cases",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<float>(
+                name: "upper_bound",
+                table: "predicted_weekly_dengue_cases",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "confidence_percentage",
+                table: "predicted_weekly_dengue_cases");
+
+            migrationBuilder.DropColumn(
+                name: "lower_bound",
+                table: "predicted_weekly_dengue_cases");
+
+            migrationBuilder.DropColumn(
+                name: "probability_of_outbreak",
+                table: "predicted_weekly_dengue_cases");
+
+            migrationBuilder.DropColumn(
+                name: "risk_level",
+                table: "predicted_weekly_dengue_cases");
+
+            migrationBuilder.DropColumn(
+                name: "upper_bound",
+                table: "predicted_weekly_dengue_cases");
+        }
+    }
+}
